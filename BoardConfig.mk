LineageOS/android_device_lge_d852

d852: rebrand to Lineage
Change-Id: I675761665666e40d256e6c222e44583d5e3fc99a

BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,7 +27,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/d852/bluetooth
 
 # Kernel
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
-TARGET_KERNEL_CONFIG := cyanogenmod_d852_defconfig
+TARGET_KERNEL_CONFIG := lineage_d852_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 TARGET_REQUIRES_BUMP := true
 

AOSiP-Devices/device_lge_d852

BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,7 +27,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/d852/bluetooth
 
 # Kernel
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
-TARGET_KERNEL_CONFIG := cyanogenmod_d852_defconfig
+TARGET_KERNEL_CONFIG := lineageos_d852_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 TARGET_REQUIRES_BUMP := true
 